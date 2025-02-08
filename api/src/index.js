dd484c4f373abc16452ff3b2928aabd8be5c8ac1
Phyo Zaw Linn <phyozawlinn1852020@gmail.com>
2025-02-08 18:22:21 +0630

    feat: add morgan for logging and update README with setup instructions

--- api/src/index.js
+++ api/src/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan")
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
 const userRouter = require("./routes/user");
 app.use(userRouter);
