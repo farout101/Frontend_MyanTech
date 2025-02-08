2747f14da88d551d0c78d3d724fa104945d5a8fe
Myatty <myintmyatps664@gmail.com>
2025-02-08 22:22:57 +0630

    frontend by ppz

---
+++ frontend/src/layouts/full/sidebar/MenuItems.jsx
@@ -0,0 +1,117 @@
+import {
+  IconListCheck, IconBasket, IconLayoutDashboard, IconHistory, IconBasketSearch, IconTruckDelivery, IconPackages, IconFileDollar
+} from '@tabler/icons-react';
+
+import { uniqueId } from 'lodash';
+
+const Menuitems = [
+  {
+    navlabel: true,
+    subheader: 'Home',
+  },
+
+  {
+    id: uniqueId(),
+    title: 'Dashboard',
+    icon: IconLayoutDashboard,
+    href: '/dashboard',
+  },
+  {
+    navlabel: true,
+    subheader: 'Product',
+  },
+  {
+    id: uniqueId(),
+    title: 'Product List',
+    icon: IconListCheck,
+    href: '/products',
+  },
+  {
+    navlabel: true,
+    subheader: 'Sale',
+  },
+  {
+    id: uniqueId(),
+    title: 'Create Sale Order',
+    icon: IconBasket,
+    href: '/sales/create',
+  },
+  {
+    id: uniqueId(),
+    title: 'Sale History',
+    icon: IconHistory,
+    href: '/sales/history',
+  },
+  {
+    id: uniqueId(),
+    title: 'Create Sale Return',
+    icon: IconBasketSearch,
+    href: '/sales/return',
+  },
+  {
+    navlabel: true,
+    subheader: 'Warehouse',
+  },
+  {
+    id: uniqueId(),
+    title: 'Pending Orders',
+    icon: IconPackages,
+    href: '/warehouse/pending',
+  },
+  {
+    id: uniqueId(),
+    title: 'Delivery Records',
+    icon: IconTruckDelivery,
+    href: '/warehouse/delivery',
+  },
+  {
+    navlabel: true,
+    subheader: 'Finance',
+  },
+  {
+    id: uniqueId(),
+    title: 'Create Invoice',
+    icon: IconFileDollar,
+    href: '/finance/invoice',
+  }
+  // {
+  //   id: uniqueId(),
+  //   title: 'Shadow',
+  //   icon: IconCopy,
+  //   href: '/ui/shadow',
+  // },
+  // {
+  //   navlabel: true,
+  //   subheader: 'Auth',
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: 'Login',
+  //   icon: IconLogin,
+  //   href: '/auth/login',
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: 'Register',
+  //   icon: IconUserPlus,
+  //   href: '/auth/register',
+  // },
+  // {
+  //   navlabel: true,
+  //   subheader: 'Extra',
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: 'Icons',
+  //   icon: IconMoodHappy,
+  //   href: '/icons',
+  // },
+  // {
+  //   id: uniqueId(),
+  //   title: 'Sample Page',
+  //   icon: IconAperture,
+  //   href: '/sample-page',
+  // },
+];
+
+export default Menuitems;
