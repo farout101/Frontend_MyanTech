5dc280588310f7f86cba0f1b76fe1655ac7b0b65
Myatty <myintmyatps664@gmail.com>
2025-02-10 13:37:49 +0630

    yearly breakup test

--- frontend/src/views/dashboard/components/YearlyBreakup.jsx
+++ frontend/src/views/dashboard/components/YearlyBreakup.jsx
@@ -2,27 +2,68 @@ import React, { useState, useEffect } from 'react';
 import Chart from 'react-apexcharts';
 import { useTheme } from '@mui/material/styles';
 import { Grid, Stack, Typography, Avatar } from '@mui/material';
-import { IconArrowUpLeft } from '@tabler/icons-react';
+import { IconArrowUpLeft, IconArrowUpRight, IconArrowDownRight } from '@tabler/icons-react';
 import axios from 'axios';
 
 import DashboardCard from '../../../components/shared/DashboardCard';
 const YearlyBreakup = () => {
   const [currentYearSale, setCurrentYearSale] = useState(0);
+  const [currentYear, setCurrentYear] = useState(0);
+  const [previousYear, setPreviousYear] = useState(0);
+  const [previousYearSale, setPreviousYearSale] = useState(0);
+  const [twoYearsAgoSale, setTwoYearsAgoSale] = useState(0);
+  const [saleIncreasePrecentage, setSaleIncreasePrecentage] = useState(0);
+
+  // useEffect(() => {
+  //   const fetchCurrentYearSale = async () => {
+  //     try {
+  //       const response = await axios.get('http://localhost:4000/api/orders/current-year-breakup');
+  //       if (response.data.length > 0) {
+  //         setCurrentYearSale(response.data[0].total_amount);
+  //       }
+  //     } catch (error) {
+  //       console.error('Error fetching total yearly sales:', error);
+  //     }
+  //   };
+
+  //   fetchCurrentYearSale();
+  // }, []);
 
   useEffect(() => {
-    const fetchCurrentYearSale = async () => {
+    const fetchYearlySales = async () => {
       try {
-        const response = await axios.get('http://localhost:4000/api/orders/current-year-breakup');
-        if (response.data.length > 0) {
-          setCurrentYearSale(response.data[0].total_amount);
+
+        const response = await axios.get('http://localhost:4000/api/orders/yearly-breakup');
+        const data = response.data;
+
+        if (data.length > 0) {
+          setCurrentYearSale(data[0].total_amount);
+          setCurrentYear(data[0].year);
         }
+        if (data.length > 1) {
+          setPreviousYearSale(data[1].total_amount);
+          setPreviousYear(data[1].year);
+          console.log(data[1].total_amount);
+        }
+        if (data.length > 2) {
+          setTwoYearsAgoSale(data[2].total_amount);
+        }
+
+        if (data.length > 1) {
+          const increasePercentage = Math.abs(((data[0].total_amount - data[1].total_amount) / data[0].total_amount) * 100);
+          setSaleIncreasePrecentage(increasePercentage);
+          console.log('Sale Increase Percentage:', increasePercentage);
+        }
+
       } catch (error) {
-        console.error('Error fetching total yearly sales:', error);
+        console.error('Error fetching yearly sales:', error);
       }
     };
 
-    fetchCurrentYearSale();
+    fetchYearlySales();
   }, []);
+
+
   // chart color
   const theme = useTheme();
   const primary = theme.palette.primary.main;
@@ -87,10 +128,14 @@ const YearlyBreakup = () => {
           </Typography>
           <Stack direction="row" spacing={1} mt={1} alignItems="center">
             <Avatar sx={{ bgcolor: successlight, width: 27, height: 27 }}>
-              <IconArrowUpLeft width={20} color="#39B69A" />
+              {currentYearSale > previousYearSale ? (
+                <IconArrowUpRight width={20} color="#39B69A" />
+              ) : (
+                <IconArrowDownRight width={20} color="#FA896B" />
+              )}
             </Avatar>
             <Typography variant="subtitle2" fontWeight="600">
-              +9%
+              {saleIncreasePrecentage.toFixed(2)}%
             </Typography>
             <Typography variant="subtitle2" color="textSecondary">
               last year
@@ -102,7 +147,7 @@ const YearlyBreakup = () => {
                 sx={{ width: 9, height: 9, bgcolor: primary, svg: { display: 'none' } }}
               ></Avatar>
               <Typography variant="subtitle2" color="textSecondary">
-                2022
+                {previousYear}
               </Typography>
             </Stack>
             <Stack direction="row" spacing={1} alignItems="center">
@@ -110,7 +155,7 @@ const YearlyBreakup = () => {
                 sx={{ width: 9, height: 9, bgcolor: primarylight, svg: { display: 'none' } }}
               ></Avatar>
               <Typography variant="subtitle2" color="textSecondary">
-                2023
+                {currentYear}
               </Typography>
             </Stack>
           </Stack>
